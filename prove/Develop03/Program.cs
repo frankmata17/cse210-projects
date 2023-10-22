b63637df12864f17c16e36cc52d82f4cef7ab24a
frankmata17 <frank.mata17@gmail.com>
2023-10-21 22:59:43 -0600

    changes

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,13 +4,10 @@ class Program
 {
     static void Main()
     {
-        // Create a Scripture object
         Scripture scripture = new Scripture("John 3:16", "For God so loved the world that He gave His only begotten Son...");
 
-        // Display the initial scripture
         scripture.Display();
 
-        // Continue hiding words until all are hidden
         while (!scripture.AllWordsHidden())
         {
             Console.WriteLine("Press Enter to hide more words or type 'quit' to exit.");
